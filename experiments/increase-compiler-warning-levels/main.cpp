938e95c984b80f6a91144ae98dac50e6cb204a7e
RidaCode <abdulredha@live.com>
2026-09-19 00:04:32 +0300

    add experiments increase compiler warning levels
    when we compile using this commandline we should get a warning:
    
    g++ main.cpp -o main -pedantic-errors -Wall -Weffc++ -Wextra
    -Wconversion -Wsign-conversion

---
+++ experiments/increase-compiler-warning-levels/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * compiler should give us warnings
+ g++ main.cpp -o main -pedantic-errors -Wall -Weffc++ -Wextra -Wconversion
+ -Wsign-conversion
+*/
+
+void foo(int) {}
+
+int main() {
+  unsigned int x{5};
+  foo(x);
+
+  return 0;
+}
