7ed94a1daef765220f6c522e2153f836e1835d73
RidaCode <abdulredha@live.com>
2026-09-19 05:16:45 +0300

    add excersise variables define, assign, and change

---
+++ exercises/variables-copy-assignment/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  int width; // declare a var named width
+
+  width = 5;          // assign the var width the value of 5
+  std::cout << width; // print out the value of the var width
+
+  width = 7;          // change the value of the variable width to 7
+  std::cout << width; // print out the value of the var width
+
+  return 0;
+}
