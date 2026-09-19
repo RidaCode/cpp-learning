eea7803bef42452611f28c4cbb92621d61d09236
RidaCode <abdulredha@live.com>
2026-09-20 00:24:18 +0300

    add exercises [[maybe_unused]] used for suppresing unused warnings
    Additionally, the compiler will likely optimize these variables out of
    the program, so they have no performance impact.
    
    The `[[maybe_unused]]` attribute should only be applied selectively to
    variables that have a specific and legitimate reason for being unused

---
+++ exercises/maybe-unused-attr/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  // v -> tell the compiler don't complain for not using these vars, which allow
+  // for betteer performance.
+  [[maybe_unused]] double a{1};
+  [[maybe_unused]] double b{2};
+  [[maybe_unused]] double c{3};
+
+  return 0;
+}
