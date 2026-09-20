1ba2b2117ecea2535be94fa40d676cd21d6dd555
RidaCode <abdulredha@live.com>
2026-09-20 07:48:03 +0300

    add exercises/iostream cin get and print two user inputs

---
+++ exercises/iostream/cin/get-two-user-input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ostream>
+
+int main() {
+  std::cout << "Enter two numbers seperated by <SPACE>: ";
+
+  int x{};
+  int y{};
+
+  std::cin >> x >> y;
+
+  std::cout << "You have enter " << x << " and " << y << '\n';
+  return 0;
+}
