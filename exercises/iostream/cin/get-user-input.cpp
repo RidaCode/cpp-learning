c8f840c3171de71d1d05c51e511a77f0bb5a469f
RidaCode <abdulredha@live.com>
2026-09-20 06:50:43 +0300

    add exercises/iostream cin get user input and print it out
    I noticed if we put a large number as an input `e.g 9999999999999999` we
    get a different number printed `2147483647` and I think at this stage,
    thats because thats the largest number the data type `int` can store, so
    we will have to use a different data type if we need a larger value.

---
+++ exercises/iostream/cin/get-user-input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  // ask the user to input
+  std::cout << "Enter a number: ";
+  // store user input
+  int userInput{};
+  std::cin >> userInput;
+  // print user input
+  std::cout << "You have entered the number: " << userInput << '\n';
+
+  return 0;
+}
