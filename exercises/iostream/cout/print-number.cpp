da8a89ed6b513246a6e16beebbd883422dbfd113
RidaCode <abdulredha@live.com>
2026-09-20 04:21:58 +0300

    add exercises iostream cout

---
+++ exercises/iostream/cout/print-number.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+  std::cout << 4; // print 4 to console
+  return 0;
+}
---
+++ exercises/iostream/cout/print-var-value.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  int x{5};       // define integer variable x, with value 5
+  std::cout << x; // print the value of variable x to console
+  return 0;
+}
