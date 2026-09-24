930f8f8478744b1e9b33c635cb3b54a47a29b495
RidaCode <abdulredha@live.com>
2026-09-24 07:24:12 +0300

    add experiments forward function declaration

---
+++ exercises/func-fwd-declartion/main.cpp
@@ -0,0 +1,13 @@
+// function declaration
+int doMath(int first, int second, int third, int fourth);
+
+int main()
+{
+    doMath(1, 2, 3, 4);
+    return 0;
+}
+
+int doMath(int first, int second, int third, int fourth)
+{
+    return first + second * third / fourth;
+}
